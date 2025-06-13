8ab2b8046bb82fa282f445d8d79792ee4373d857
etawil <33093447+etawil@users.noreply.github.com>
2025-06-13 12:52:12 +0300

    Add files via upload
    V 2.0.0

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ resource "random_string" "myrandom" {
   upper = false
 }
 
-resource "azurerm_resource_group" "ET-TF-RG" {
+resource "azurerm_resource_group" "Prod-Deployment-RG" {
   name = "${var.resource_group_name}${random_string.myrandom.id}"
   location = var.location
 }
 
-resource "azurerm_storage_account" "mystoragestaticweb" {
+resource "azurerm_storage_account" "deplmtstg" {
   name                     = "${var.storage_account_name}${random_string.myrandom.id}"
-  resource_group_name      = azurerm_resource_group.ET-TF-RG.name
+  resource_group_name      = azurerm_resource_group.Prod-Deployment-RG.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -22,8 +22,8 @@ resource "azurerm_storage_account" "mystoragestaticweb" {
   }
 }
 
-resource "azurerm_storage_account_static_website" "mystaticweb" {
-  storage_account_id = azurerm_storage_account.mystoragestaticweb.id
+resource "azurerm_storage_account_static_website" "staticwebsite" {
+  storage_account_id = azurerm_storage_account.deplmtstg.id
   error_404_document = "error.html"
   index_document     = "index.html"
 }
